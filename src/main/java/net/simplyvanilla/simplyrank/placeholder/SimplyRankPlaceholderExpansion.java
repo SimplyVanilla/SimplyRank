64f5b800edd03962dac51b55dd6e7ebc69b921f2
Netherwhal <mail@netherwhal.com>
2022-06-26 23:23:49 +0200

    0.3.1

--- src/main/java/net/simplyvanilla/simplyrank/placeholder/SimplyRankPlaceholderExpansion.java
+++ src/main/java/net/simplyvanilla/simplyrank/placeholder/SimplyRankPlaceholderExpansion.java
@@ -25,7 +25,7 @@ public class SimplyRankPlaceholderExpansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "0.3.0";
+        return "0.3.1";
     }
 
     @Override
