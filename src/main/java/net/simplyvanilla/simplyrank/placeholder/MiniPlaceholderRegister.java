2ca927b551100f14b5da8357ebf1e8de0a96c8ba
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-10 19:46:12 +0200

    chore: cleanup

--- src/main/java/net/simplyvanilla/simplyrank/placeholder/MiniPlaceholderRegister.java
+++ src/main/java/net/simplyvanilla/simplyrank/placeholder/MiniPlaceholderRegister.java
@@ -10,11 +10,8 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.minimessage.tag.Tag;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.simplyvanilla.simplyrank.SimplyRankPlugin;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
@@ -27,66 +24,72 @@ public class MiniPlaceholderRegister {
     }
 
     public void register() {
-        {
-            Expansion.Builder builder = Expansion.builder("simplyrank");
+        registerRankExpansion();
+        registerScoreboardExpansion();
+    }
 
-            builder.filter(audience -> audience instanceof Player)
-                .audiencePlaceholder("code_color", (audience, queue, ctx) -> {
-                    Player player = (Player) audience;
-                    try {
-                        TextColor color = plugin.getDataManager().loadGroupDataSync(
-                                plugin.getDataManager().loadPlayerDataSync(player.getUniqueId())
+    private static void registerScoreboardExpansion() {
+        Expansion.Builder builder = Expansion.builder("simplyscoreboard");
+
+        builder.filter(Player.class::isInstance)
+            .audiencePlaceholder("team_name", (audience, queue, ctx) -> {
+                Player player = (Player) audience;
+                return Tag.selfClosingInserting(
+                    Component.text(getTeamValue(player, Team::getName, "")));
+            })
+            .audiencePlaceholder("team_color", (audience, queue, ctx) -> {
+                Player player = (Player) audience;
+                return Tag.styling(style -> style.color(
+                    getTeamValue(player, Team::color, NamedTextColor.WHITE)));
+            })
+            .audiencePlaceholder("team_prefix", (audience, queue, ctx) -> {
+                Player player = (Player) audience;
+                return Tag.inserting(getTeamValue(player, Team::prefix, Component.text("")));
+            })
+            .audiencePlaceholder("team_suffix", (audience, queue, ctx) -> {
+                Player player = (Player) audience;
+                return Tag.inserting(getTeamValue(player, Team::suffix, Component.text("")));
+            });
+
+        builder.build().register();
+    }
+
+    private void registerRankExpansion() {
+        Expansion.Builder builder = Expansion.builder("simplyrank");
+
+        builder.filter(Player.class::isInstance)
+            .audiencePlaceholder("code_color", (audience, queue, ctx) -> {
+                Player player = (Player) audience;
+                try {
+                    TextColor color = plugin.getDataManager().loadGroupDataSync(
+                            plugin.getDataManager().loadPlayerDataSync(player.getUniqueId())
+                                .getPrimaryGroup())
+                        .getColor();
+                    return Tag.styling(builder1 -> builder1.color(color));
+                } catch (IOException ignored) {
+                    ignored.printStackTrace();
+                }
+                return TagsUtils.EMPTY_TAG;
+            })
+            .audiencePlaceholder("prefix", (audience, queue, ctx) -> {
+                try {
+                    return Tag.inserting(
+                        miniMessage().deserialize(plugin.getDataManager().loadGroupDataSync(
+                                plugin.getDataManager()
+                                    .loadPlayerDataSync(((Player) audience).getUniqueId())
                                     .getPrimaryGroup())
-                            .getColor();
-                        return Tag.styling(builder1 -> builder1.color(color));
-                    } catch (IOException ignored) {
-                    }
-                    return TagsUtils.EMPTY_TAG;
-                })
-                .audiencePlaceholder("prefix", (audience, queue, ctx) -> {
-                    try {
-                        return Tag.inserting(
-                            miniMessage().deserialize(plugin.getDataManager().loadGroupDataSync(
-                                    plugin.getDataManager()
-                                        .loadPlayerDataSync(((Player) audience).getUniqueId())
-                                        .getPrimaryGroup())
-                                .getPrefix()));
-                    } catch (IOException ignored) {
-                    }
-                    return TagsUtils.EMPTY_TAG;
-                })
-                .audiencePlaceholder("primary_rank",
-                    (audience, queue, ctx) -> Tag.inserting(Component.text(plugin.getDataManager()
-                        .loadPlayerDataSync(((Player) audience).getUniqueId())
-                        .getPrimaryGroup())));
+                            .getPrefix()));
+                } catch (IOException ignored) {
+                    ignored.printStackTrace();
+                }
+                return TagsUtils.EMPTY_TAG;
+            })
+            .audiencePlaceholder("primary_rank",
+                (audience, queue, ctx) -> Tag.inserting(Component.text(plugin.getDataManager()
+                    .loadPlayerDataSync(((Player) audience).getUniqueId())
+                    .getPrimaryGroup())));
 
-            builder.build().register();
-        }
-        {
-            Expansion.Builder builder = Expansion.builder("simplyscoreboard");
-
-            builder.filter(audience -> audience instanceof Player)
-                .audiencePlaceholder("team_name", (audience, queue, ctx) -> {
-                    Player player = (Player) audience;
-                    return Tag.selfClosingInserting(
-                        Component.text(getTeamValue(player, Team::getName, "")));
-                })
-                .audiencePlaceholder("team_color", (audience, queue, ctx) -> {
-                    Player player = (Player) audience;
-                    return Tag.styling(style -> style.color(
-                        getTeamValue(player, Team::color, NamedTextColor.WHITE)));
-                })
-                .audiencePlaceholder("team_prefix", (audience, queue, ctx) -> {
-                    Player player = (Player) audience;
-                    return Tag.inserting(getTeamValue(player, Team::prefix, Component.text("")));
-                })
-                .audiencePlaceholder("team_suffix", (audience, queue, ctx) -> {
-                    Player player = (Player) audience;
-                    return Tag.inserting(getTeamValue(player, Team::suffix, Component.text("")));
-                });
-
-            builder.build().register();
-        }
+        builder.build().register();
     }
 
     private static <T> T getTeamValue(Player player, Function<Team, T> f, T def) {
