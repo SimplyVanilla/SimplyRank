b3d52e5b2cc752351f09b694b8dfda1ac4ff8d7a
Emmanuel Lampe <mail@emmanuel-lampe.de>
2023-06-22 16:46:56 +0200

    feat: clear player data cache on quit

--- src/main/java/net/simplyvanilla/simplyrank/data/DataManager.java
+++ src/main/java/net/simplyvanilla/simplyrank/data/DataManager.java
@@ -121,6 +121,6 @@ public final class DataManager {
   }
 
   public void invalidatePlayerData(UUID uniqueId) {
-      this.playerDataCache.remove(uniqueId);
+    this.playerDataCache.remove(uniqueId);
   }
 }
