80047c4125d88a1d8e980bbfbe4394c60d24615d
Netherwhal <netherwhal@simplyvanilla.net>
2024-01-21 13:44:02 +0100

    Prep 0.7.0 (#75)

--- src/main/java/net/simplyvanilla/simplyrank/placeholder/ScoreboardTeamsPlaceholderExtension.java
+++ src/main/java/net/simplyvanilla/simplyrank/placeholder/ScoreboardTeamsPlaceholderExtension.java
@@ -28,7 +28,7 @@ public class ScoreboardTeamsPlaceholderExtension extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "0.6.6";
+        return "0.7.0";
     }
 
     @Override
